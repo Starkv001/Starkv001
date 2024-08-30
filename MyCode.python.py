6336438b18f1bf0378c7f892200e4737f68d2000
Starkv001 <aaronadefemi24@gmail.com>
2024-08-30 05:51:26 +0100

    MyCode.Python.py

---
+++ MyCode.python.py
@@ -0,0 +1,18 @@
+#Ask for your name 
+name = input ("What is your name? ")
+
+#Ask for your age
+age = int(input ("How old are you? "))
+
+#Ask for your faculty 
+faculty = input ("Your faculty or school of? ")
+
+#Ask for your department 
+department = input ("your department? ")
+
+#Ask for your course code 
+course = input ("Your course code? ")
+
+#print a message 
+print ("Hello, " + name + "! You are " + str(age) + " years old, from faculty or school of " + faculty + " in department of " + str(department) + ". Your course code is " + str(course) + ".")
+
